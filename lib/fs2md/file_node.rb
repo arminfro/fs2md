e4d532efcfe6a413e88e9341762079ea74394580
Armin Fröhlich <mail@arminfroehlich.de>
2020-04-13 13:00:21 +0200

    add FileNodeContentParser

--- lib/fs2md/file_node.rb
+++ lib/fs2md/file_node.rb
@@ -8,64 +8,52 @@ class FileNode < Node
   end
 
   def read
-    f_content     = File.read(@file).split("\n")
-    io_with_index = with_index(strip_array(f_content))
-    io            = io_with_index.map { |a| a[0] }
-
-    node_indices = io_with_index.map do |l_i|
-      l                       = l_i[0]
-      i                       = l_i[1]
-      last_ele                = i == io.size - 1
-      is_headline             = l =~ /\A#/
-      starts_without_headline = i == 0 && !is_headline
-
-      i if is_headline || last_ele || starts_without_headline
-    end.compact
-
-    tt = node_indices[..-2].map do |i|
-      starts_without_headline = i.zero? && io[i] !~ /\A#/
-      if starts_without_headline
-        headline   = name(:beautiful)
-        node_depth = depth
-      else
-        count      = io[i].count('#')
-        node_depth = depth + count
-        headline   = io[i].sub('#', '').strip
-      end
-      range                   = (i + 1)..node_indices[node_indices.index(i) + 1]
-      last                    = range.last == io.size - 1
-      content                 = strip_array(io[(starts_without_headline ? range.first - 1 : range.first)..(last ? range.last : range.last - 1)]).join("\n")
-      TextNode.new(headline, content, node_depth, path, self)
-    end
-
-    if tt.empty?
-      [TextNode.new(name(:beautiful), io.join("\n"), depth, path, self)]
-    elsif name(:beautiful) == tt.first.name
-      tt
-    else
-      tt.unshift(TextNode.new(name(:beautiful), '', depth, path, self))
-    end
-    # todo, sort_by(&:name)
+    FileNodeContentParser.new(File.read(@file).split("\n"), @path, name(:beautiful), depth).parse
   end
 
   def output_filename
     File.join(super, @name + (Node.config[:print_beamer] ? '_beamer' : ''))
   end
 
-  def with_index(io)
-    side_effect_arr = []
-    io.each_with_index { |l, i| side_effect_arr.push([l, i]) }
-    side_effect_arr
-  end
-
-  # strip array of empty pre and post strings
-  def strip_array(arr)
-    first_line_index = nil
-    last_line_index  = nil
-    arr.each_with_index do |el, i|
-      first_line_index = i if !el.empty? && first_line_index.nil?
-      last_line_index  = i if (arr[i + 1].nil? || arr.size - 1 == i) && !el.empty?
+  FileNodeContentParser = Struct.new(:content, :path, :name, :depth) do
+    def parse
+      text_nodes = node_indices_to_content_arr(text_node_indices)
+      if text_nodes.empty?
+        [TextNode.new(name, content.join("\n"), depth, path, self)]
+      elsif name == text_nodes.first.name
+        text_nodes
+      else
+        text_nodes.unshift(TextNode.new(name, '', depth, path, self))
+      end
     end
-    arr[first_line_index..last_line_index]
+
+    def text_node_indices
+      content.each_with_index.map do |line, index|
+        last_ele                = index == content.size - 1
+        is_headline             = line =~ /\A#/
+        starts_without_headline = index.zero? && !is_headline
+
+        index if is_headline || last_ele || starts_without_headline
+      end.compact
+    end
+
+    def node_indices_to_content_arr(indices)
+      indices[..-2].map do |index|
+        starts_without_headline = index.zero? && content[index] !~ /\A#/
+        if starts_without_headline
+          headline   = name
+          node_depth = depth
+        else
+          count      = content[index].count('#')
+          node_depth = depth + count
+          headline   = content[index].sub('#', '').strip
+        end
+
+        range        = (index + 1)..indices[indices.index(index) + 1]
+        last         = range.last == content.size - 1
+        text_content = content[(starts_without_headline ? range.first - 1 : range.first)..(last ? range.last : range.last - 1)].join("\n")
+        TextNode.new(headline, text_content, node_depth, path, self)
+      end
+    end
   end
 end
