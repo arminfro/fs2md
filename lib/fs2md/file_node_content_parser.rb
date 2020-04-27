f7d201aa003cbe4ef1d29e7da34e73befdc42aa7
Armin Fröhlich <mail@arminfroehlich.de>
2020-04-27 15:07:37 +0200

    fix bug in FileNodeContentParser

--- lib/fs2md/file_node_content_parser.rb
+++ lib/fs2md/file_node_content_parser.rb
@@ -2,7 +2,7 @@
 
 FileNodeContentParser = Struct.new(:content, :path, :name, :depth, :file_node) do
   def parse
-    text_nodes = node_indices_to_content_arr(text_node_indices)
+    text_nodes = node_indices_to_text_node_arr(text_node_indices)
     if text_nodes.empty?
       [TextNode.new(name, content.join("\n"), depth, path, file_node)]
     elsif name == text_nodes.first.name
@@ -17,26 +17,27 @@ FileNodeContentParser = Struct.new(:content, :path, :name, :depth, :file_node) d
       last_ele                = index == content.size - 1
       is_headline             = line =~ /\A#/
       starts_without_headline = index.zero? && !is_headline
-
-      index if is_headline || last_ele || starts_without_headline
+      if last_ele && is_headline # edge case, empty headline without body
+        index.positive? ? index - 1 : 0
+      elsif is_headline || last_ele || starts_without_headline
+        index
+      end
     end.compact
   end
 
-  def node_indices_to_content_arr(indices)
+  def node_indices_to_text_node_arr(indices)
     indices[..-2].map do |index|
       starts_without_headline = index.zero? && content[index] !~ /\A#/
       if starts_without_headline
         headline   = name
         node_depth = depth
       else
-        count      = content[index].count('#')
-        node_depth = depth + count
         headline   = content[index].sub('#', '').strip
+        node_depth = depth + 1
       end
-
-      range        = (index + 1)..indices[indices.index(index) + 1]
-      last         = range.last == content.size - 1
-      text_content = content[(starts_without_headline ? range.first - 1 : range.first)..(last ? range.last : range.last - 1)].join("\n")
+      range                   = (index + 1)..indices[indices.index(index) + 1]
+      last                    = range.last == content.size - 1
+      text_content            = content[(starts_without_headline ? range.first - 1 : range.first)..(last ? range.last : range.last - 1)].join("\n")
       TextNode.new(headline, text_content, node_depth, path, file_node)
     end
   end
